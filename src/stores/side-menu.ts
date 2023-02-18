7b003f641286e611e12a92ed33f86e48d254a05a
Edivan de Castro <edivandecastro@gmail.com>
2023-02-18 06:41:47 -0300

    Adicionado menu de segundo nivel

--- src/stores/side-menu.ts
+++ src/stores/side-menu.ts
@@ -1,13 +1,13 @@
 import { MenuService } from '@/services/MenuService'
 import { baseUrl } from '@/services/Api';
 import { defineStore } from "pinia";
-import { Menu } from "@/models/Menu"
+import { Menu } from "@/models/Menu";
 
 export interface MenuApi {
   icon: string;
   title: string;
   page_name?: string;
-  sub_menu?: Menu[];
+  submenus?: MenuApi[];
   ignore?: boolean;
 }
 
@@ -22,7 +22,11 @@ let menuService = new MenuService(baseUrl.atlas_api_v1);
 await menuService.getMenus()
   .then(response => {
     response.data.menus.forEach((menu: MenuApi) => {
-      menus.menu.push(new Menu(menu.icon, menu.title, menu.page_name))
+      let submenus: Menu[] = []
+      menu.submenus?.forEach((menu: MenuApi) => {
+        submenus.push(new Menu(menu.icon, menu.title, menu.page_name))
+      })
+      menus.menu.push(new Menu(menu.icon, menu.title, menu.page_name, submenus))
     });
   })
   .catch(error => {
