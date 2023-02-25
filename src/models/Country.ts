7697258fd6bd290a93fbde16ace6b12caaad8431
Edivan de Castro <edivandecastro@gmail.com>
2023-02-25 08:12:40 -0300

    Implementado model para country

---
+++ src/models/Country.ts
@@ -0,0 +1,47 @@
+import { CountryApi } from "@/utils/interfaces";
+
+export class Country {
+  private id: string
+  private name: string
+  private locale: string
+  private acronym: string
+
+  constructor({ id, name, locale, acronym }: CountryApi) {
+    this.id = id;
+    this.name = name;
+    this.locale = locale;
+    this.acronym = acronym;
+  }
+
+  public getId(): string {
+    return this.id;
+  }
+
+  public setId(id: string): void {
+    this.id = id;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public setName(name: string): void {
+    this.name = name;
+  }
+
+  public getLocale(): string {
+    return this.locale;
+  }
+
+  public setLocale(locale: string): void {
+    this.locale = locale;
+  }
+
+  public getAcronym(): string {
+    return this.acronym;
+  }
+
+  public setAcronym(acronym: string): void {
+    this.acronym = acronym;
+  }
+}
