728b7c61d60d23637e057b59f926e2dfe2e8f3fd
Edivan de Castro <edivandecastro@gmail.com>
2023-03-02 11:30:45 -0300

    Adicionado o  i18n no escopo da aplicação

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from "pinia";
+import { i18n } from "@/locales/i18n";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/css/app.css";
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+createApp(App).use(i18n).use(router).use(createPinia()).mount('#app')
