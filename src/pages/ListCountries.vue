cbdc3b3b16562cc1989687c0e86df6b52c6aff13
Edivan de Castro <edivandecastro@gmail.com>
2023-03-02 06:42:04 -0300

    Page para listar countries

---
+++ src/pages/ListCountries.vue
@@ -0,0 +1,201 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Button from "@/base-components/Button";
+import Lucide from "@/base-components/Lucide";
+import { Dialog, Menu } from "@/base-components/Headless";
+import { FormInput, FormSelect } from "@/base-components/Form";
+import Table from "@/base-components/Table";
+import fakerData from "@/utils/faker";
+import _ from "lodash";
+import Tippy from "@/base-components/Tippy";
+import Pagination from "@/base-components/Pagination";
+import { useRouter } from "vue-router";
+import { CountryService } from "@/services/CountryService";
+import { baseUrl } from "@/services/Api";
+import { reactive, onMounted } from "vue";
+import { CountryApi } from "@/utils/interfaces";
+import { Country } from "@/models/Country";
+
+const deleteConfirmationModal = ref(false);
+const setDeleteConfirmationModal = (value: boolean) => {
+  deleteConfirmationModal.value = value;
+};
+const deleteButtonRef = ref(null);
+const router = useRouter();
+
+const onAddNewCountry = () => {
+  router.push({ name: "new-location" });
+};
+
+let countryService = new CountryService(baseUrl.atlas_api_v1);
+let countries: Array<Country> = reactive([]);
+
+onMounted(() => {
+  countryService
+    .all()
+    .then((response) => {
+      response.data.countries.forEach((attributes: CountryApi) => {
+        countries.push(new Country(attributes));
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+</script>
+
+<template>
+  <h2 class="mt-10 text-lg font-medium intro-y">Países</h2>
+  <div class="grid grid-cols-12 gap-6 mt-5">
+    <div class="flex flex-wrap items-center col-span-12 mt-2 intro-y sm:flex-nowrap">
+      <Button variant="primary" class="mr-2" :elevated="true" @click="onAddNewCountry">
+        Adicionar Novo País
+      </Button>
+      <Menu>
+        <Menu.Button :as="Button" class="px-2 !box">
+          <span class="flex items-center justify-center w-5 h-5">
+            <Lucide icon="Plus" class="w-4 h-4" />
+          </span>
+        </Menu.Button>
+        <Menu.Items class="w-40">
+          <Menu.Item> <Lucide icon="Printer" class="w-4 h-4 mr-2" /> Print </Menu.Item>
+          <Menu.Item> <Lucide icon="FileText" class="w-4 h-4 mr-2" /> Export to Excel </Menu.Item>
+          <Menu.Item> <Lucide icon="FileText" class="w-4 h-4 mr-2" /> Export to PDF </Menu.Item>
+        </Menu.Items>
+      </Menu>
+      <div class="hidden mx-auto md:block text-slate-500">Mostrando 1 a 10 de 150 entradas</div>
+      <div class="w-full mt-3 sm:w-auto sm:mt-0 sm:ml-auto md:ml-0">
+        <div class="relative w-56 text-slate-500">
+          <FormInput type="text" class="w-56 pr-10 !box" placeholder="Procurar..." />
+          <Lucide icon="Search" class="absolute inset-y-0 right-0 w-4 h-4 my-auto mr-3" />
+        </div>
+      </div>
+    </div>
+    <div class="col-span-12 overflow-auto intro-y lg:overflow-visible">
+      <Table class="border-spacing-y-[10px] border-separate -mt-2">
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th class="border-b-0 whitespace-nowrap"> Bandeira </Table.Th>
+            <Table.Th class="border-b-0 whitespace-nowrap"> Nome </Table.Th>
+            <Table.Th class="border-b-0 whitespace-nowrap"> Localidade </Table.Th>
+            <Table.Th class="border-b-0 whitespace-nowrap"> Acrônimo </Table.Th>
+            <Table.Th class="text-center border-b-0 whitespace-nowrap"> Ações </Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>
+          <Table.Tr v-for="country in countries" :key="country.getId()" class="intro-x">
+            <Table.Td
+              class="first:rounded-l-md last:rounded-r-md w-40 bg-white border-b-0 dark:bg-darkmode-600 shadow-[20px_3px_20px_#0000000b]"
+            >
+              <div class="flex">{{ country.getName() }}</div>
+            </Table.Td>
+            <Table.Td
+              class="first:rounded-l-md last:rounded-r-md bg-white border-b-0 dark:bg-darkmode-600 shadow-[20px_3px_20px_#0000000b]"
+            >
+              <div class="font-medium whitespace-nowrap">{{ country.getName() }}</div>
+            </Table.Td>
+            <Table.Td
+              class="first:rounded-l-md last:rounded-r-md text-center bg-white border-b-0 dark:bg-darkmode-600 shadow-[20px_3px_20px_#0000000b]"
+            >
+              <div class="flex items-center mr-3 text-slate-500" href="#">
+                {{ country.getLocale() }}
+              </div>
+            </Table.Td>
+            <Table.Td
+              class="first:rounded-l-md last:rounded-r-md w-40 bg-white border-b-0 dark:bg-darkmode-600 shadow-[20px_3px_20px_#0000000b]"
+            >
+              <div class="flex items-center mr-3 text-slate-500">
+                {{ country.getAcronym() }}
+              </div>
+            </Table.Td>
+            <Table.Td
+              class="first:rounded-l-md last:rounded-r-md w-56 bg-white border-b-0 dark:bg-darkmode-600 shadow-[20px_3px_20px_#0000000b] py-0 relative before:block before:w-px before:h-8 before:bg-slate-200 before:absolute before:left-0 before:inset-y-0 before:my-auto before:dark:bg-darkmode-400"
+            >
+              <div class="flex items-center justify-center">
+                <a class="flex items-center mr-3" href="">
+                  <Lucide icon="CheckSquare" class="w-4 h-4 mr-1" />
+                  Edit
+                </a>
+                <a
+                  class="flex items-center text-danger"
+                  href="#"
+                  @click="
+                    (event) => {
+                      event.preventDefault();
+                      setDeleteConfirmationModal(true);
+                    }
+                  "
+                >
+                  <Lucide icon="Trash2" class="w-4 h-4 mr-1" /> Delete
+                </a>
+              </div>
+            </Table.Td>
+          </Table.Tr>
+        </Table.Tbody>
+      </Table>
+    </div>
+    <div class="flex flex-wrap items-center col-span-12 intro-y sm:flex-row sm:flex-nowrap">
+      <Pagination class="w-full sm:w-auto sm:mr-auto">
+        <Pagination.Link>
+          <Lucide icon="ChevronsLeft" class="w-4 h-4" />
+        </Pagination.Link>
+        <Pagination.Link>
+          <Lucide icon="ChevronLeft" class="w-4 h-4" />
+        </Pagination.Link>
+        <Pagination.Link>...</Pagination.Link>
+        <Pagination.Link>1</Pagination.Link>
+        <Pagination.Link active>2</Pagination.Link>
+        <Pagination.Link>3</Pagination.Link>
+        <Pagination.Link>...</Pagination.Link>
+        <Pagination.Link>
+          <Lucide icon="ChevronRight" class="w-4 h-4" />
+        </Pagination.Link>
+        <Pagination.Link>
+          <Lucide icon="ChevronsRight" class="w-4 h-4" />
+        </Pagination.Link>
+      </Pagination>
+      <FormSelect class="w-20 mt-3 !box sm:mt-0">
+        <option>10</option>
+        <option>25</option>
+        <option>35</option>
+        <option>50</option>
+      </FormSelect>
+    </div>
+  </div>
+
+  <Dialog
+    :open="deleteConfirmationModal"
+    @close="
+      () => {
+        setDeleteConfirmationModal(false);
+      }
+    "
+    :initialFocus="deleteButtonRef"
+  >
+    <Dialog.Panel>
+      <div class="p-5 text-center">
+        <Lucide icon="XCircle" class="w-16 h-16 mx-auto mt-3 text-danger" />
+        <div class="mt-5 text-3xl">Are you sure?</div>
+        <div class="mt-2 text-slate-500">
+          Do you really want to delete these records? <br />
+          This process cannot be undone.
+        </div>
+      </div>
+      <div class="px-5 pb-8 text-center">
+        <Button
+          variant="outline-secondary"
+          type="button"
+          @click="
+            () => {
+              setDeleteConfirmationModal(false);
+            }
+          "
+          class="w-24 mr-1"
+        >
+          Cancel
+        </Button>
+        <Button variant="danger" type="button" class="w-24" ref="deleteButtonRef"> Delete </Button>
+      </div>
+    </Dialog.Panel>
+  </Dialog>
+</template>
